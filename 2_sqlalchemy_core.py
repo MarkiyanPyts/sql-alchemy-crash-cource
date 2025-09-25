37e07f535a0669e1f33d9ab9fea2c79e6d1e7d4f
Markiyan Pyts <markiyan.pyts@gmail.com>
2025-09-25 12:47:04 +0300

    Update 2_sqlalchemy_core.py

--- 2_sqlalchemy_core.py
+++ 2_sqlalchemy_core.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Float, ForeignKey
 from dotenv import load_dotenv
 import os
 
@@ -16,9 +16,28 @@ people = Table(
     Column('age', Integer, nullable=False),
 )
 
+things = Table(
+    'things',
+    meta,
+    Column('id', Integer, primary_key=True),
+    Column('description', String, nullable=False),
+    Column('value', Float, nullable=False),
+    Column('owner_id', Integer, ForeignKey('people.id'))
+)
+
 meta.create_all(engine)
 
 conn = engine.connect()
-insert_statement = people.insert().values(name='Mike', age=30)
-result = conn.execute(insert_statement)
+insert_statement = people.insert().values(name='Jane', age=40)
+select_statement = people.select().where(people.c.age > 30)
+conn.execute(insert_statement)
+result = conn.execute(select_statement)
+for row in result.fetchall():
+    print(row)
+
+update_statement = people.update().where(people.c.name == 'Jane').values(age=39)
+conn.execute(update_statement)
+
+delete_statement = people.delete().where(people.c.name == 'Jane')
+conn.execute(delete_statement)
 conn.commit()
