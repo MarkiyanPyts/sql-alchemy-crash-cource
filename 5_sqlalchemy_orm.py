52978f3886f9d7e7670475068213b54cccefdd1b
Markiyan Pyts <markiyan.pyts@gmail.com>
2025-09-25 14:16:10 +0300

    Update 5_sqlalchemy_orm.py

--- 5_sqlalchemy_orm.py
+++ 5_sqlalchemy_orm.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey, func
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 from dotenv import load_dotenv
 import os
@@ -29,6 +29,36 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-new_person = Person(name='Sam', age=29)
+new_person = Person(name='Charlie', age=70)
 session.add(new_person)
+# session.flush()  # Flush to get the new person's ID temporarily without committing
+
+new_thing = Thing(description='Watch', value=199.99, person=new_person)
+session.add(new_thing)
 session.commit()
+
+print("new person: ")
+print(new_person.name)
+print("new person things: ")
+for thing in new_person.things:
+    print(thing.description)
+
+tuples_result = session.query(Person.name, Person.age).all()
+for row in tuples_result:
+    print(row)
+
+objects_result = session.query(Person).filter(Person.age > 30).all()
+print([p.name for p in objects_result])
+
+things_result = session.query(Thing).filter(Thing.value != 100).all()
+print([t.description for t in things_result])
+
+update_person_query = session.query(Person).filter(Person.name == 'Charlie').update({'name': 'John'})
+
+aggregate_query = session.query(Thing.owner, func.sum(Thing.value)).group_by(Thing.owner).having(func.sum(Thing.value) > 100)
+for row in aggregate_query:
+    print(row)
+
+#delete_things_query = session.query(Thing).filter(Thing.description == 'Watch').delete()
+session.commit()
+session.close()
